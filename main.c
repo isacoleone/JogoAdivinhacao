a7f3adbd973ead3a592c20e2a15ef66dfa5804a9
isa <isacoleone27@gmail.com>
2024-09-11 20:09:02 -0300

    Jogo Finalizado

--- main.c
+++ main.c
@@ -3,17 +3,17 @@
 #include <time.h>
 
 int main() {
-	int jogarNovamente;
+	int jogarNovamente= 1; // Flag para jogar novamente
 
 	while (jogarNovamente) {
 		printf("************************************\n");
 		printf("* Bem-vindo ao Jogo de Adivinhacao *\n");
 		printf("************************************\n\n");
 
-		int segundos = time(0);  //pegando os segundos da data
-		srand(segundos);        //fazendo com que o numero gerado pelo rand() seja diferente
+		int segundos = time(0);   // Pega os segundos da data atual
+		srand(segundos);        // Inicializa o gerador de números aleatórios
 		                
-		int numeroSecreto = rand() % 100;
+		int numeroSecreto = rand() % 100;   // Gera um número de 0 a 99
 		int ganhou = 0;
 		int chute;
 		double pontos = 1000;
@@ -27,6 +27,7 @@ int main() {
 		printf("Escolha: ");
 		scanf("%d", &dificuldade);
 
+		// Define o número de tentativas com base na dificuldade escolhida
 		switch (dificuldade) {
 			case 1:
 				tentativas = 20;
@@ -39,30 +40,35 @@ int main() {
 				break;
 		}
 
+		// Loop para o jogador usar as tentativas
 		for (int i = 1; i <= tentativas; i++) {
 
 			printf("-> Tentativa %d de %d\n", i, tentativas);
 			printf("Chute um numero: ");
-			printf("\n");
 			scanf("%d", &chute);
 		
 			int ultimoChute;
 			int acertou = chute == numeroSecreto;
 			int maior = chute > numeroSecreto;
-			int menor = chute < numeroSecreto;	            //tambem poderia usar o (double) para converter
-			double pontosPerdidos = abs(chute - numeroSecreto) / 2.0;	 //abs() transforma o numero em positivo
+			int menor = chute < numeroSecreto;	           
+			double pontosPerdidos = abs(chute - numeroSecreto) / 2.0;	// Calcule os pontos perdidos
 			pontos = pontos - pontosPerdidos;
 
+			// Verifica se o chute foi um número negativo
 			if (chute < 0) {
 				printf("Nao e permitido valores negativos, tente novamente\n\n");
 				i--;
 				continue;
 			}
+
+			// Verifica se o chute foi igual ao chute anterior
 			if (ultimoChute == chute) {
 				printf("Voce chutou esse numero na ultima tentativa, tente novamente\n\n");
 				i--;
 				continue;
 			}
+
+			// Verifica se o chute é igual ao número secreto
 			if (acertou) {
 				ganhou = 1;
 				break;
@@ -70,15 +76,20 @@ int main() {
 			} else {
 				printf("Voce errou!\n");
 
+				// Verifica se o chute foi maior
 				if (maior) {
 					printf("Seu chute foi maior do que o numero secreto, voce perdeu %.2f pontos\n\n", pontosPerdidos);
 				}
+
+				// Verifica se o chute foi menor
 				if (menor) {
 					printf("Seu chute foi menor do que o numero secreto, voce perdeu %.2f pontos\n\n", pontosPerdidos);
 				}
 			}
 			ultimoChute = chute;
 		}
+
+		// Mensagem final de acordo com o resultado
 		if (ganhou) {
 			printf("Parabens, voce acertou!\n");
 			printf("Voce fez %.2f pontos\n", pontos);
@@ -86,6 +97,9 @@ int main() {
 		} else {
 			printf("Voce perdeu!\n");
 		}
+
+		// Pergunta se o jogador deseja jogar novamente
+		printf("\n");
 		printf("Digite [1] se deseja jogar novamente\n");
 		scanf("%d", &jogarNovamente);
 
